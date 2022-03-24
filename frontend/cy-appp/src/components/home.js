9e56a7c9e6012f91f772df598a6f961113b461ab
pauline12ish34 <paulineishimwe12@gmail.com>
2022-03-24 17:28:45 +0200

    front end components

---
+++ frontend/cy-appp/src/components/home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Home=()=>{
+return(
+<>
+<div>
+<div className='grid grid-cols-3  text-white home'> 
+  <div className="pl-8 pt-24  mt-8">
+  <h1 className='font-bold text-3xl pl-10 pb-5'>Hi , I am  Pauline</h1>
+  <p className='h-12 w-52  justify-between'>Welcome to the best Electricity online selling</p>
+  </div>
+
+  <div className="pt-5">
+    <div className='mt-96 -mb-8 ml-14 '>
+  <button className="text-3xl text-white bg-green h-14 w-64 rounded-3xl">GET Startes</button>
+  </div>
+  </div>
+  <div className="pt-5 mt-8">
+  <img src="watt.webp" alt="pauline's meter" className='ml-20'/>
+  </div>
+ </div>
+
+</div>
+
+
+</>);
+}
+
+export default Home;
+
---
+++ frontend/cy-appp/src/components/viewDays/viewtransactions.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+
+
+ 
+  export default class View extends React.Component {
+
+    state = {
+        transactions: []
+      }
+    
+      componentDidMount() {
+        axios.get("http://localhost:8080")
+          .then(res => {
+            const transactions = res.data;
+            this.setState({ transactions });
+          })
+      }
+    
+      render() {
+        return (
+            <>
+            <table >
+                <thead>
+                    <tr>
+                    <th>token</th>
+                    <th>meter numbre</th>
+                    <th>money paid</th>
+                    <th>remaining Days</th>
+                    </tr>
+                </thead>
+  <tr>
+                {
+                    
+          this.state.persons
+            .map(transaction =>
+              <td key={transaction.token}>{transaction.token},{transaction.meter}, {transaction.money} , {transaction.status}, {transaction.dateExp}</td>
+            )
+        }</tr>
+            </table>
+            </>
+        )}
+    }
